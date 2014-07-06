17740c16af1da3f2890cca6ab1dd6ecc1fefd5af
alianpaul chen <alianpaul@localhost.localdomain>
2014-07-06 21:23:17 +0800

    Lab4 partA finished

---
+++ lab3/buflab-handout/test.d
@@ -0,0 +1,11 @@
+
+test.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	b8 b8 1f f2 65       	mov    $0x65f21fb8,%eax
+   5:	8d 6c 24 24          	lea    0x24(%esp),%ebp
+   9:	68 c1 8d 04 08       	push   $0x8048dc1
+   e:	c3                   	ret    
---
+++ lab3/buflab-handout/test.s
@@ -0,0 +1,4 @@
+mov $0x65f21fb8,%eax
+leal 0x24(%esp),%ebp
+push $0x08048dc1
+ret
